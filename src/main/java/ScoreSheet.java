e0849ed059a74e3042f064c8c0e8ab1424522438
Dave Ellis <dave@ellisnetmail.co.uk>
2018-01-29 15:47:48 +0000

    Refactored ScoreSheet class to put hashmap setup in a separate method

--- src/main/java/ScoreSheet.java
+++ src/main/java/ScoreSheet.java
@@ -8,14 +8,18 @@ public class ScoreSheet {
 
     public ScoreSheet(){
         this.scores = new HashMap<>();
+        this.setupScoresHashMap();
+        this.upperSectionScore = 0;
+        this.totalScore = 0;
+    }
+
+    public void setupScoresHashMap(){
         this.scores.put("Ones", 0);
         this.scores.put("Twos", 0);
         this.scores.put("Threes", 0);
         this.scores.put("Fours", 0);
         this.scores.put("Fives", 0);
         this.scores.put("Sixes", 0);
-        this.upperSectionScore = 0;
-        this.totalScore = 0;
     }
 
     public HashMap<String, Integer> getScores() {
