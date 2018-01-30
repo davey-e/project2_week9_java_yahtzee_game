d69702af69dd8e296db8c8317d2a119d9c5287af
Dave Ellis <dave@ellisnetmail.co.uk>
2018-01-30 10:56:23 +0000

    Added winner attribute to Game class with getter and test

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,12 +3,18 @@ import java.util.ArrayList;
 public class Game {
 
     private ArrayList<Player> players;
+    private Player winner;
 
     public Game() {
         this.players = new ArrayList<>();
+        this.winner = null;
     }
 
     public ArrayList<Player> getPlayers() {
         return this.players;
+    }
+
+    public Player getWinner() {
+        return winner;
     }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class GameTest {
 
@@ -15,5 +16,10 @@ public class GameTest {
     @Test
     public void playersArrayListStartsEmpty(){
         assertEquals(0, game.getPlayers().size());
+    }
+
+    @Test
+    public void winnerStartsNull(){
+        assertNull(game.getWinner());
     }
 }
